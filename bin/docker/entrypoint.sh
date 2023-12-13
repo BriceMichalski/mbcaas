5569e0d5a8f12e41cd8049542431072fd5f9bd10
Brice MICHALSKI <brice@myerp.com>
2023-12-13 21:51:12 +0100

    MBCaaS toolkit container & mbenv cli
    mbenv dockerfile
    
    delete telport in dev
    
    mbcaas-toolkit container
    mbenv

--- bin/docker/entrypoint.sh
+++ bin/docker/entrypoint.sh
@@ -9,7 +9,7 @@ fi
 
 tsh kube login teleport.mbcaas.com  2>&1 > /dev/null
 tsh proxy kube -p 443  2>&1 > /dev/null &
-sleep 1
+sleep 2
 echo -e "\n"
 
 export KUBECONFIG=/home/mbcaas-tools/.tsh/keys/teleport.mbcaas.com/$MBCAAS_USER-kube/teleport.mbcaas.com/localproxy-443-kubeconfig
