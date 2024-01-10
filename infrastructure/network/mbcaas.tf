6f1ed9b6c1a5921857e2a25a72a824ba57517743
Brice MICHALSKI <brice@myerp.com>
2024-01-10 18:32:55 +0100

    raspberry pi inegration

--- infrastructure/network/mbcaas.tf
+++ infrastructure/network/mbcaas.tf
@@ -67,6 +67,11 @@ resource "unifi_device" "udm_se" {
         port_profile_id     = resource.unifi_port_profile.mbcaas_no_poe.id
     }
 
+    port_override {
+        name                = "Port 8 - mbcaas-pi-01"
+        number              = 8
+        port_profile_id     = resource.unifi_port_profile.mbcaas_poe.id
+    }
 
 }
 
